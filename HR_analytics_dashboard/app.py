b85bbfc2f0f2e6560bea4419b540069c9f56114d
Prathiba R <rprathiba86@gmail.com>
2026-01-31 11:50:58 +0530

    Update app.py

--- HR_analytics_dashboard/app.py
+++ HR_analytics_dashboard/app.py
@@ -8,11 +8,9 @@ st.set_page_config(page_title="HR Analytics Dashboard", layout="wide")
 sns.set_style("whitegrid")
 
 # ---------------- LOAD DATA ----------------
-@st.cache_data
-def load_data():
-    return pd.read_csv("../hr_cleaned.csv")
+ 
 
-df = load_data()
+df = pd.read_csv("hr_cleaned.csv")
 
 # ---------------- TITLE ----------------
 st.title(" HR Analytics – Employee Attrition Dashboard")
